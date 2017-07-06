8d98fcd4333d6ead45e14d245c3322877d25ddc9
Alvin Cheng <ascheng21@gmail.com>
2017-07-06 10:12:02 -0700

    Gave each note an id property. #1

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const notes = []
+let noteID = 1
 const app = express()
 
 app.use(bodyParser.json())
 
 app.post('/notes', (req, res) => {
+  req.body.id = noteID++
   notes.push(req.body)
   res.sendStatus(201)
 })
