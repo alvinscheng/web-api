f75d00289ab257519315c9440d659c4a3f64a44d
Alvin Cheng <ascheng21@gmail.com>
2017-07-06 09:28:46 -0700

    Added a post handler on the express app. #1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const notes = []
+const app = express()
+
+app.use(bodyParser.json)
+
+app.post('/notes', (req, res) => {
+  notes.push(req.body)
+  res.sendStatus(201)
+})
+
+app.listen(3000, () => console.log('Listening on 3000!'))
